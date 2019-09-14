7aa07183ad48f9fca9452501410fcdb2c4c75dd1
Phillip Stockmann <mail@phillip-stockmann.de>
2019-09-14 14:08:14 +0200

    unified whitespaces according to editorconfig

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ require('colors');
 
 global.__base = __dirname;
 
-var path 	= require('path');
-var reader 	= require( path.join( global.__base, "lib", "reader.coffee" ) )();
+var path   = require('path');
+var reader = require( path.join( global.__base, "lib", "reader.coffee" ) )();
